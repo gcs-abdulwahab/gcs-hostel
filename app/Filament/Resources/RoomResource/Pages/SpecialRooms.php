8b97632ff60da879a7b82a26ae29f00d2e647780
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2023-10-22 17:13:23 +0500

    Update SpecialRooms.php

--- app/Filament/Resources/RoomResource/Pages/SpecialRooms.php
+++ app/Filament/Resources/RoomResource/Pages/SpecialRooms.php
@@ -9,5 +9,10 @@ class SpecialRooms extends Page
 {
     protected static string $resource = RoomResource::class;
 
+    public static function getNavigationLabel(): string
+    {
+        return 'Custom Navigation Label';
+    }
+
     protected static string $view = 'filament.resources.room-resource.pages.special-rooms';
 }
